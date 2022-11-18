4efa22b22def5a9effbf112b5ad6d8b846053df1
waltonnya <117782227+waltonnya@users.noreply.github.com>
2022-11-17 21:43:38 -0600

    first

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
 // write your code here
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+
+{
+    int i, rnum, amount;
+   
+    cout<<" Enter N amount of numbers to generate ";
+    cin>>amount;
+   
+    srand(time(0));
+    for(i=0; i<amount; i++)
+    {
+        rnum = rand()%(100)+1;
+        cout<<endl<<rnum;
+    }
+    cout<<endl;
+    return 0;
+}
